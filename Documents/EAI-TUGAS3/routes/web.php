77c5515f0bb9e9cc534081bb33256d211b76e699
Putrinadia23 <putrinadia@student.telkomuniversity.ac.id>
2024-06-02 20:03:10 +0700

    tugas3

--- Documents/EAI-TUGAS3/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\APIFootballController;
-
-Route::get('/', [APIFootballController::class, 'index'])->name('dashboard');
-Route::get('/teams/{leagueId}', [APIFootballController::class, 'teams'])->name('teams');
-Route::get('/standings/{leagueId}', [APIFootballController::class, 'standings'])->name('standings');
